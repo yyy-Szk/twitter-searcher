e9ecdada13fd608b902dd9d94582739e71eb8839
Yoshimasa Suzuki <cognac0004@gmail.com>
2022-09-11 17:54:15 +0900

    Twitter検索用のエンドポイント（仮）を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
+  before_action :validate, only: :search_customers
+
   def index
+    @results = []
+  end
+
+  # TODO: 設計に迷ったので、一時的に追加。後ほど別のコントローラーに切り出したい
+  def search_customers
+    # TODO: 一時的な対応。後ほどログインさせて、ログインしているユーザーの情報から持ってくる
+    access_token = Rails.application.credentials.twitter[:bearer_token]
+    twitter_searcher = TwitterSearcher.new(access_token: access_token)
+
+    params[:conditions].each do |index, value|
+      p index, value["search_type"], value["content"]
+    end
+
+    @results = []
+
+    render "home/index"
+  end
+
+  def validate
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get "search_customers", to: 'home#search_customers'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
