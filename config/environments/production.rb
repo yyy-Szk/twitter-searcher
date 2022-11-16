3a28eeff6f317a5d725a8c8e4cc1c854951a188d
Yoshimasa Suzuki <cognac0004@gmail.com>
2022-11-16 22:55:35 +0900

    setting for production hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.hosts << ".herokuapp.com"
+  config.hosts << ".onrender.com"
 end
